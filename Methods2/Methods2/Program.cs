2f9b5069b69d407d5c6da8c96cb7b83e9918f780
apetrisc2003 <apetrisc2003@gmail.com>
2019-11-16 12:38:42 +0200

    dfddfg

---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a number:");
+            string firstNumber = Console.ReadLine();
+
+            Console.WriteLine("Please write another number:");
+            string lastNumber = Console.ReadLine();
+
+            int number1 = int.Parse(firstNumber);
+            int number2= int.Parse(lastNumber);
+
+            Console.WriteLine("The sum is :"+ (number1 + number2)  + "?!!");
+        }
+    }
+}
